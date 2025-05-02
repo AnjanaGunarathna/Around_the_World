9694d9b0ba8c74481f47a972688078d357cd9191
Anjana Gunarathna <152806674+AnjanaGunarathna@users.noreply.github.com>
2025-05-02 15:37:38 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "process.env.REACT_APP_API_URL",
+    baseURL: "https://around-the-world-w6y5.onrender.com",
     withCredentials: true,
 });
 
